fb298480cdc77a221988aac45975adccf6920213
Javier Jimenez and Sarah Lindmar <javier@>
2017-10-25 16:28:06 -0700

    update to-do project with mutiple components and pipes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-root',
   template: `
     <div class="container">
-     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-     <h3>{{currentFocus}}</h3>
-
-     <ul>
-       <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
-     </ul>
-
-   </div>
+      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+      <h3>{{currentFocus}}</h3>
+      <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
+      <hr>
+      <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
+      <new-task (newTaskSender)="addTask($event)"></new-task>
+    </div>
   `
 })
 
@@ -21,15 +21,24 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
+  selectedTask = null;
+
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-}
 
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
 
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string) { }
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
+  }
+
 }
